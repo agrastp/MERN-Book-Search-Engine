a20fe7725ea6f5cc6ab6772086dd25cfb4f07afa
AgrastP <agrastp@yahoo.com>
2024-02-13 14:56:46 -0500

    server

--- server/server.js
+++ server/server.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { ApolloServer } = require('@apollo/server');
+const { ApolloServer } = require('apollo-server-express');
 const { expressMiddleware } = require('@apollo/server/express4');
 const path = require('path');
 const { authMiddleware } = require('./utils/auth');
