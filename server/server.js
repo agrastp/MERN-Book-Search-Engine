3042ec711ac57a748af947ce38dec99eb091ca18
AgrastP <agrastp@yahoo.com>
2024-02-13 15:54:47 -0500

    removed commented out script

--- server/server.js
+++ server/server.js
@@ -49,15 +49,3 @@ startApolloServer();
 
 
 
-
-
-// if we're in production, serve client/build as static assets
-// if (process.env.NODE_ENV === 'production') {
-//   app.use(express.static(path.join(__dirname, '../client/build')));
-// }
-
-// app.use(routes);
-
-// db.once('open', () => {
-//   app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
-// });
